e3f696f4992d29bf0f6c2f56ee4bf1e95a5b9323
Phantomdye <benni9902@web.de>
2018-04-14 21:43:32 +0200

    Updated Bot

--- embed.js
+++
@@ -1,8 +0,0 @@
-const COLOR = {
-    red: 0xe74c3c,
-    green: 0x2ecc71,
-    yellow: 0xf1c40f,
-    orange: 0xe67e22,
-    blue: 0x3498db,
-    purple: 0x9b59b6
-}
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js")
 const bot			= new Discord.Client({disableEveryone: true})
 const config 	= require('./config.json')
-const Embeds 	= require('./embed.js')
+const embed 	= require('./embed.json')
 
 bot.on("ready", async () => {
 	console.log(`Logged in as ${bot.user.username}...`)
@@ -27,7 +27,7 @@ bot.on("message", async message => {
 	if (cmd === `${prefix}botinfo`) {
 		let botembed = new Discord.RichEmbed()
 		.setDescription("Bot Information")
-		.setColor(Embeds.green)
+		.setColor(embed.green)
 		.addField("Bot Name: ", bot.user.username);
 
 		return message.channel.send(botembed);
