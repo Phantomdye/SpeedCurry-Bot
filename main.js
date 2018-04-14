df204816d1500c3ab22eb1da466bbd7e89a7c608
Phantomdye <benni9902@web.de>
2018-04-14 21:02:34 +0200

    Updated Bot

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js")
 const bot			= new Discord.Client({disableEveryone: true})
 const config 	= require('./config.json')
-const Embeds 	= require('./embed.json')
+const Embeds 	= require('./embed.js')
 
 bot.on("ready", async () => {
 	console.log(`Logged in as ${bot.user.username}...`)
