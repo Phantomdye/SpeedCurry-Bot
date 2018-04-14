547ea51efbb22a1aebf5a26b4caca39be9ab035e
Phantomdye <benni9902@web.de>
2018-04-14 20:56:14 +0200

    Update
    bla

--- main.js
+++ main.js
@@ -1,29 +1,38 @@
-const Discord 	= require("discord.js")
-const bot		= new Discord.Client({disableEveryone: true})
-const prefix 	= "::"
+const Discord = require("discord.js")
+const bot			= new Discord.Client({disableEveryone: true})
+const config 	= require(',/config.json')
 const Embeds 	= require('./embeds')
 
 bot.on("ready", async () => {
 	console.log(`Logged in as ${bot.user.username}...`)
-	bot.user.setActivity('Hey, whats up!')
+	bot.user.setActivity('Hey! ::botinfo')
 });
 
 bot.on("message", async message => {
-	var cont = msg.cont,
-		author = msg.member,
-		chan = msg.channel,
-		guild = msg.guild
-	
 	if (message.author.bot) return
 	if (message.channel.type === "dm") return
-	
+
+	let prefix = config.prefix
 	let messageArray = message.content.split(" ")
 	let cmd = messageArray[0]
 	let args = messageArray.slice(1)
-	
+
+//Commands
 	if (cmd === `${prefix}ping`) {
 		message.channel.send("Pong!")
 	}
+	if (cmd === `${prefix}hello`) {
+		message.channel.send("Hey!")
+	}
+	if (cmd === `${prefix}botinfo`) {
+		let botembed = new Discord.RichEmbed()
+		.setDescription("Bot Information")
+		.setColor(Embeds.green)
+		.addField("Bot Name: ", bot.user.username);
+
+		return message.channel.send(botembed);
+	}
+
 });
 
 bot.login(process.env.token)
