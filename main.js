5b0725c2b650031bcb80074c72d3b332f1306e7b
Phantomdye <benni9902@web.de>
2018-04-14 15:30:56 +0200

    Updated Bot

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
-const Discord = require("discord.js")
-const bot = new Discord.Client({disableEveryone: true});
+const Discord 	= require("discord.js")
+const bot 		= new Discord.Client({disableEveryone: true})
+const fs		= require('fs')
+
+const config = JSON.parse(fs.readFileSync('config.josn', 'utf8'))
 
 bot.on("ready", async () => {
-	console.log('${bot.user.username} is online!');
-	bot.user.setActivity('Hey, whats up!');
+	console.log(`Logged in as ${bot.user.username}...`)
+	bot.user.setActivity('Hey, whats up!')
 });
 
 bot.on("message", async message => {
-	if (message.author.bot) return;
-	if (message.channel.type === "dm") return;
+	if (message.author.bot) return
+	if (message.channel.type === "dm") return
 	
-	let prefix = '::';
-	let messageArray = message.content.split(" ");
-	let cmd = messageArray[0];
-	let args = messageArray.slice(1);
+	let messageArray = message.content.split(" ")
+	let cmd = messageArray[0]
+	let args = messageArray.slice(1)
 	
-	if (cmd === '${prefix}ping') {
-		message.channel.send("Pong!");
+	if (cmd === `${config.prefix}ping`) {
+		message.channel.send("Pong!")
 	}
 });
 
-bot.login(process.env.token);
+bot.login(process.env.token)
